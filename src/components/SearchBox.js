a0ab95701621254b3d1787be3a839af8c0fa21ad
unknown <hamzahmitou@gmail.com>
2019-08-23 12:17:51 +0300

    Styling the searchbox component

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,8 +3,10 @@ import React from 'react';
 
 const SearchBox = ({searchChange}) => {
     return(
-        <input type="search" placeholder="Enter name" onChange={searchChange} />
-    )
+        <div className="pa2">
+            <input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="Enter name" onChange={searchChange} />
+        </div>
+        )
 }
 
 export default SearchBox;
